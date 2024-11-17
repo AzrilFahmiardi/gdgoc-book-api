49cfc17b1168baefdb4acf4c239cbab9a517e458
AzrilFahmiardi <anakijummira@gmail.com>
2024-11-17 16:55:01 +0700

    another import typo

--- src/components/BookGrid.jsx
+++ src/components/BookGrid.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import BookCard from "./bookCard";
+import BookCard from "./BookCard";
 
 const url = import.meta.env.VITE_APP_URL || 'http://localhost:8000';
 
